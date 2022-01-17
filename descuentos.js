c432e5a71a5818e0be189d0b5ef79c27546fbc6c
bleyner99 <bleynercochero@gmail.com>
2022-01-17 12:16:43 -0500

    cambios en archivos html y js

--- descuentos.js
+++ descuentos.js
@@ -10,17 +10,48 @@ function calcularPrecioConDescuento(precio, descuento) {
 }
 
 function onClickButtonPriceDiscount() {
+  const coupons = [
+      "NNSS",
+      "RRTT",
+      "PPP",
+    
+  ];
+
   const inputPrice = document.getElementById("InputPrice");
   const priceValue = inputPrice.value;
   
-  const inputDiscount = document.getElementById("InputDiscount");
-  const discountValue = inputDiscount.value;
+  const inputCoupon = document.getElementById("InputCoupon");
+  const couponValue = inputCoupon.value;
 
-  const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+  let descuento;
+
+  switch(couponValue) {
+    case coupons[0]: // "NNSS"
+      descuento = 15;
+    break;
+    case coupons[1]: // "RRTT"
+      descuento = 30;
+    break;
+    case coupons[2]: // "PPP"
+      descuento = 25;
+    break;
+  }
+  
+
+  const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
   const resultP = document.getElementById("ResultP");
-  resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  if(coupons.includes(couponValue)){
+    resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+  }else{
+    alert("cupón inválido")
+  }
+  
+ 
+
 }
+
+  
 
 
 // console.log({
