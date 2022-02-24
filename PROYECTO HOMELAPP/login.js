49f88acb308df07b2d2f3fc45d72daa5a2eb5564
Bleyner99 <bleynercochero@gmail.com>
2022-02-24 10:49:22 -0500

    Nuevo proyecto hommelapp

---
+++ PROYECTO HOMELAPP/login.js
@@ -0,0 +1,13 @@
+function ir(){
+
+var c=2468;
+var u="NAME";
+
+if(document.form.password.value==c && document.form.login.value==u){
+    alert("Bienvenido");
+
+}else{
+    alert("Ingrese usuario y contraeña incorrecta");
+}
+}
+
