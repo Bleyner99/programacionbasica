e2e19c394cdd41cdabbd25d475da83de7f09e9bd
Bleyner99 <bleynercochero@gmail.com>
2022-02-24 15:35:01 -0500

    adelantando proyecto

--- PROYECTO HOMELAPP/login.js
+++ PROYECTO HOMELAPP/login.js
@@ -1,13 +1,23 @@
 function ir(){
 
-var c=2468;
-var u="NAME";
+var c=123456;
+var u="bleyner";
 
-if(document.form.password.value==c && document.form.login.value==u){
+
+if(document.form.password.value=="" && document.form.login.value== ""){
+
+    alert("Digite campos vacíos");
+
+}else if(document.form.password.value==c && document.form.login.value==u){
     alert("Bienvenido");
 
-}else{
-    alert("Ingrese usuario y contraeña incorrecta");
+window.location="inicio.html"}   else{
+    alert("usuario y contraeña incorrecta");
 }
 }
 
+if(document.getElementById="informe"){
+
+alert("hola");
+
+}
