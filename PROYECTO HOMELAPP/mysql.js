95db45acb355970b871dc68f1e6b92b3e1742288
Bleyner99 <bleynercochero@gmail.com>
2022-02-25 14:11:29 -0500

    conexion con base de datos

---
+++ PROYECTO HOMELAPP/mysql.js
@@ -0,0 +1,40 @@
+const mysql = require('mysql')
+
+const conection = mysql.createConnection({
+    host: 'localhost',
+    user: 'Bleyner',
+    password: '123456',
+    database: 'usuarios',
+
+})
+
+conection.connect( (err) =>{
+    if(err) throw err
+    console.log('La conexión funciona')
+   
+})
+
+
+const insertar = "INSERT INTO login (id,nombre) VALUES(NULL, 'ALDAX')"
+conection.query(insertar,(err, rows)=>{
+    if(err) throw err
+
+})
+
+
+conection.query('SELECT * from login', (err, rows) => {
+  if(err) throw err
+  console.log('Los datos de la tabla son estos') 
+  console.log(rows)
+  console.log(rows)
+  console.log('La cantidad de resultados es:')
+  console.log(rows.length)
+  const usuarioUno = rows  [0]
+  console.log(`El usuario se llama ${usuarioUno.nombre} y tiene el id ${usuarioUno.id}`)
+
+})
+
+
+conection.end()
+ 
+
